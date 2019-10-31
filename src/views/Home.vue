fb2663d35e4911d1d4241fdf5e1f4cf79319c5e0
Nathaniel Lin <linlilulll@gmail.com>
2019-10-31 18:02:36 +0800

    small fix of styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Shuttle logo" src="../assets/logo.svg" width="80px" height="80px" />
     <h1>Shuttle -- Deliver your data, through the cloud, to the world, instantly</h1>
-    <st-card :header="'some title'" :content="'somecontent'" :footer="'somefooter'" />
+    <st-card header="'some title'" content="'somecontent'" footer="'somefooter'" />
   </div>
 </template>
 
